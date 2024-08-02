46ca8ba705b7168f82ccd132f41a4eb227432d20
Diana Grayden <graydendi@mail.ru>
2024-08-02 09:38:40 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let stroka = "";
+    const ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    const teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    const hungreds = ["", "one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"];
+
+    if (number === 0) return "zero";
+    
+    if (Math.floor(number / 100) > 0) {
+        stroka += hungreds[Math.floor(number / 100)] + " ";
+        number %= 100;
+    }
+
+    if (number > 10 && number < 20) {
+        stroka += teens[number - 10] + " ";
+    } else {
+        if (Math.floor(number / 10) > 0) {
+            stroka += tens[Math.floor(number / 10)] + " ";
+            number %= 10;
+        }
+        
+        if (number > 0) {
+            stroka += ones[number] + " ";
+        }
+    }
+
+    return stroka.trim();
 }
